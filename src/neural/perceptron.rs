2b611cf48544d0878e28b0a084735a54f6be3240
vspecky <thelonenerd9913@gmail.com>
2020-07-03 18:19:20 +0530

    Completed perceptron 90%. Maybe I'll add a separate 'verbose' o/p generator class

--- src/neural/perceptron.rs
+++ src/neural/perceptron.rs
@@ -6,6 +6,7 @@ use crate::{
     utils::{
         EncephalonError,
         Tensor,
+        TrainParams,
     },
     funcs::{
         cost::{self, CostFns},
@@ -16,7 +17,6 @@ use crate::{
 use rand::prelude::*;
 
 type EError = EncephalonError;
-type TensorType = Vec<Vec<f32>>;
 
 pub struct Perceptron {
     inputs: u32,
@@ -31,11 +31,11 @@ impl Perceptron {
             return Err(EError::new("A perceptron cannot have zero amount of inputs or outputs"));
         }
 
-        let mut weights: TensorType = Vec::new();
+        let mut weights = Vec::<Vec<f32>>::with_capacity((out + 1) as usize);
 
         let mut rng = rand::thread_rng();
         for _ in 0..out {
-            let mut w: Vec<f32> = Vec::new();
+            let mut w  = Vec::<f32>::with_capacity((inp + 1) as usize);
 
             for _ in 0..=inp {
                 w.push(rng.gen::<f32>());
@@ -59,26 +59,67 @@ impl Perceptron {
             return Err(EError::new("Size of data provided does not match input size"));
         }
 
-        let res = data.mult(&self.weights.transpose())?;
+        let prepared_data = data.insert_cols_front(vec![1.]);
+
+        let res = prepared_data.mult(&self.weights.transpose())?;
 
         Ok(res)
     }
 
-    pub fn fit(&mut self, input: &Tensor, output: &Tensor, lr: f32) -> Result<(), EError> {
-        let x = input.insert_cols_front(vec![1.]);
+    fn fit(&mut self, inputs: &Tensor, outputs: &Tensor, params: &TrainParams) -> Result<(), EError> {
+        let x = inputs.insert_cols_front(vec![1.]);
 
-        let predicted = self.predict(&input)?;
+        let predicted = self.predict(inputs)?;
 
-        let w_gradients = match self.cost {
-            _ => cost::d_mse_gradient(&x, predicted, output)?
-        };
+        let w_gradients = cost::d_mse_gradient(&x, predicted, outputs)?;
 
-        self.weights = self.weights.sub(&w_gradients.mult_scalar(lr))?;
+        self.weights = self.weights.sub(&w_gradients.mult_scalar(params.lr))?;
 
         Ok(())
     }
 
-    pub fn train(&self, x: &Tensor, y: &Tensor, lr: f32, gd: GradientDescent) -> Result<(), EError> {
-        unimplemented!();
+    pub fn train(&mut self, params: TrainParams) -> Result<(), EError> {
+        for _ in 0..params.epochs {
+            match params.gd {
+                GradientDescent::Batch => {
+                    self.fit(&params.inputs, &params.outputs, &params)?;
+                }
+                
+                GradientDescent::Stochastic => {
+                    for (inp, out) in params.inputs.mat.iter().zip(&params.outputs.mat) {
+                        let inp_tensor = Tensor::new(vec![inp.clone()])?;
+                        let out_tensor = Tensor::new(vec![out.clone()])?;
+
+                        self.fit(&inp_tensor, &out_tensor, &params)?;
+                    }
+                }
+
+                GradientDescent::MiniBatch(batch_size) => {
+                    if params.inputs.mat.len() / batch_size as usize == 0 {
+                        return Err(EError::new("Cannot split inputs into that many batches"));
+                    }
+                    
+                    let batches = (params.inputs.mat.len() as f32 / batch_size as f32).ceil();
+                    
+                    for batch in 0..batches as u32 {
+                        let mut inp = Vec::<Vec<f32>>::with_capacity(batch_size as usize);
+                        let mut out = Vec::<Vec<f32>>::with_capacity(batch_size as usize);
+
+                        for (i, o) in params.inputs.mat.iter().skip(batch as usize * batch_size as usize)
+                            .zip(params.outputs.mat.iter().skip(batch as usize * batch_size as usize)) {
+                            inp.push(i.clone());
+                            out.push(o.clone());
+                        }
+
+                        let inp_tens = Tensor::new(inp)?;
+                        let out_tens = Tensor::new(out)?;
+
+                        self.fit(&inp_tens, &out_tens, &params)?;
+                    }
+                }
+            }
+        }
+
+        Ok(())
     }
 }
